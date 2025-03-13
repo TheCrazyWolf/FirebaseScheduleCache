824e9201ee70c170a12ec312587457fa62cfd4a3
kulagin <alex_kulagin@samgk.ru>
2025-03-13 11:26:26 +0400

    Доп пакеты + изменения

--- SamgkCacheSchedule/Worker.cs
+++ SamgkCacheSchedule/Worker.cs
@@ -1,5 +1,8 @@
 using ClientSamgk;
+using ClientSamgk.Models;
+using ClientSamgk.Models.Enums.Schedule;
 using Firebase.Database;
+using Newtonsoft.Json;
 
 namespace SamgkCacheSchedule;
 
@@ -19,10 +22,28 @@ public class Worker : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-        
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            
+            try
+            { 
+                var query = new ScheduleQuery()
+                    .WithAllForSearchType(ScheduleSearchType.Employee)
+                    .WithDelay(0)
+                    .WithDate(DateOnly.FromDateTime(DateTime.Now));
+                
+                var schedule = await _clientSamgkApi.Schedule.GetScheduleAsync(query, stoppingToken);
+                var json = JsonConvert.SerializeObject(schedule);
+                await _firebaseClient
+                    .Child(DateOnly.FromDateTime(DateTime.Now).ToString("yy-MM-dd"))
+                    .PutAsync(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
             await Task.Delay(1000, stoppingToken);
         }
     }
